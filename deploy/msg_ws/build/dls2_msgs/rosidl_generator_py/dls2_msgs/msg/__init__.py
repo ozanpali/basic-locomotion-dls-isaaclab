c56e02908679ede92af2ca413c4a9e186ba58d80
giulioturrisi <giulioturrisi@hotmail.it>
2025-07-15 11:52:07 +0200

    cleaning

--- deploy/msg_ws/build/dls2_msgs/rosidl_generator_py/dls2_msgs/msg/__init__.py
+++
@@ -1,4 +0,0 @@
-from dls2_msgs.msg._base_state_msg import BaseStateMsg  # noqa: F401
-from dls2_msgs.msg._blind_state_msg import BlindStateMsg  # noqa: F401
-from dls2_msgs.msg._control_signal_msg import ControlSignalMsg  # noqa: F401
-from dls2_msgs.msg._trajectory_generator_msg import TrajectoryGeneratorMsg  # noqa: F401
