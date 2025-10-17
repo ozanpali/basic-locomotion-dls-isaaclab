6a7a9b15db8a0704995fe072f03e358f5499e40f
Ozan Pali <ozanpali@gmail.com>
2025-10-17 22:56:36 +0200

    individual airtime FLRR 0.5 FRRL 0.3 FL calf target -2.5 maintenance bend swing

--- source/basic_locomotion_dls_isaaclab/basic_locomotion_dls_isaaclab/tasks/robust/aliengo_env_cfg.py
+++ source/basic_locomotion_dls_isaaclab/basic_locomotion_dls_isaaclab/tasks/robust/aliengo_env_cfg.py
@@ -411,11 +411,11 @@ class AliengoFlatEnvCfg(DirectRLEnvCfg):
 
     # Front-left specific rewards (moved here so they are grouped with other feet rewards)
     # Reward for maintaining a desired height with the front-left (FL) foot
-    fl_height_maintenance_reward_scale = 0.25 * (1-use_amp) * 0.0
+    fl_height_maintenance_reward_scale = 0.25 * (1-use_amp)
     # Reward for encouraging the FL calf joint to bend (more negative angle)
-    fl_calf_bend_reward_scale = 0.25 * (1-use_amp) * 0.0
+    fl_calf_bend_reward_scale = 0.25 * (1-use_amp)
     # Desired height above the standard/default foot height for FL (meters)
-    front_left_always_swing_reward_scale = 0.20 * (1-use_amp) * 0.0
+    front_left_always_swing_reward_scale = 0.20 * (1-use_amp)
     # Per-foot air time reward scales (allow different weighting per leg)
     feet_air_time_FL_reward_scale = 0.5 * (1-use_amp)
     feet_air_time_FR_reward_scale = 0.5 * (1-use_amp)
