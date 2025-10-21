cca9818d3020f585147bde0b5167c13c91e1e39c
Ozan Pali <ozanpali@gmail.com>
2025-10-21 11:51:46 +0200

    Rigt pc run code

--- scripts/train_all_commits.sh
+++ scripts/train_all_commits.sh
@@ -65,10 +65,10 @@ done
 
 # Ordered list from first to last commit (as you provided)
 COMMITS=(
-  010589280c102df668da4136aa487aca261e3630
-  7d2d9e1d3d84d6e3dd7bfbaa6e6f9e4fd429e98a
-  #65704bf678cc32977412f9d3cf9c7df5c7f812ec
-  #a47e70f9b1aade1825fa8598b1125c708309c9ec
+  #010589280c102df668da4136aa487aca261e3630
+  #7d2d9e1d3d84d6e3dd7bfbaa6e6f9e4fd429e98a
+  65704bf678cc32977412f9d3cf9c7df5c7f812ec
+  a47e70f9b1aade1825fa8598b1125c708309c9ec
 )
 
 # --- Helpers ---
