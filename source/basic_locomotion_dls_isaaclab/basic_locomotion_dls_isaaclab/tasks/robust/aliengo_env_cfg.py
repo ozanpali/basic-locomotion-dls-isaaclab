dfe8e39826039d6d5b0d1d4c533406a70f0af1cc
Ozan Pali <ozanpali@gmail.com>
2025-10-26 23:05:36 +0100

    3 leg attempt 0-20seconds

--- source/basic_locomotion_dls_isaaclab/basic_locomotion_dls_isaaclab/tasks/robust/aliengo_env_cfg.py
+++ source/basic_locomotion_dls_isaaclab/basic_locomotion_dls_isaaclab/tasks/robust/aliengo_env_cfg.py
@@ -134,21 +134,21 @@ class EventCfg:
     torque_scale_FL_hip = EventTerm(
         func=custom_events.scale_joint_torque,
         mode="interval",
-        interval_range_s=(25.0, 35.0),
+        interval_range_s=(0.0, 20.0),
         params={"asset_cfg": SceneEntityCfg("robot", joint_names=["FL_hip_joint"]), "scale": 0.3},
     )
 
     torque_scale_FL_thigh = EventTerm(
         func=custom_events.scale_joint_torque,
         mode="interval",
-        interval_range_s=(25.0, 35.0),
+        interval_range_s=(0.0, 20.0),
         params={"asset_cfg": SceneEntityCfg("robot", joint_names=["FL_thigh_joint"]), "scale": 0.3},
     )
 
     torque_scale_FL_calf = EventTerm(
         func=custom_events.scale_joint_torque,
         mode="interval",
-        interval_range_s=(25.0, 35.0),
+        interval_range_s=(0.0, 20.0),
         params={"asset_cfg": SceneEntityCfg("robot", joint_names=["FL_calf_joint"]), "scale": 0.3},
     )
     """
