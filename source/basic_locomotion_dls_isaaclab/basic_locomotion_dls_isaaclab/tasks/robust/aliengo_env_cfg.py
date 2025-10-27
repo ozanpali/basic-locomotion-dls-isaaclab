4c37b008b8e43e0ed5967089299eed35f6c375b2
Ozan Pali <ozanpali@gmail.com>
2025-10-27 12:17:26 +0100

    4Leg_OnlyObservationisAwareofEvent

--- source/basic_locomotion_dls_isaaclab/basic_locomotion_dls_isaaclab/tasks/robust/aliengo_env_cfg.py
+++ source/basic_locomotion_dls_isaaclab/basic_locomotion_dls_isaaclab/tasks/robust/aliengo_env_cfg.py
@@ -426,6 +426,7 @@ class AliengoFlatEnvCfg(DirectRLEnvCfg):
     #feet_air_time_FR_reward_scale = 0.5 * (1-use_amp) * 0.0
     #feet_air_time_RL_reward_scale = 0.5 * (1-use_amp) * 0.0
     #feet_air_time_RR_reward_scale = 0.5 * (1-use_amp) * 0.0
+    
     #reward in the air punish when touch the ground
     feet_air_time_FL_failure_reward_scale = 0.5 * (1-use_amp) * 0.0
     feet_air_time_RL_failure_reward_scale = 0.5 * (1-use_amp) * 0.0
